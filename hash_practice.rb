506d9fc8011ad4d0ba6c8a34c9515f79dd229366
Tyler Caudill <teecaud@hotmail.com>
2022-04-21 18:12:10 -0500

    Pairing excersise 1

---
+++ hash_practice.rb
@@ -0,0 +1,16 @@
+person = {
+  "birth_year" => 1988,
+  "favorite color" => "purple",
+  "student" => true,
+  "location" => "Chicago"
+}
+
+person["favorite_sport"] = "football"
+person["favorite_food"] = "steak"
+
+p person
+
+p person["birth_year"]
+
+person.keys
+person.values
